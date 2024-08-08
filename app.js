132e9ac8b1b17b81ee07d9d2dd5ce0f01202c3e8
ScottSyverson <166549266+ScottSyverson@users.noreply.github.com>
2024-08-08 11:11:46 -0500

    cleaning code
    just used the shift+alt+f to make the code prettier

--- app.js
+++ app.js
@@ -29,35 +29,35 @@ let totalQuartersValue = quartersRollValue + quartersValue;
 
 let totalChange = totalPenniesValue + totalNickelsValue + totalDimesValue + totalQuartersValue;
 
-while (pennies > 50){
-    pennies = pennies -50;
-    penniesRoll ++;
+while (pennies > 50) {
+    pennies = pennies - 50;
+    penniesRoll++;
 }
-console.log(penniesRoll + " rolls of pennies." );
+console.log(penniesRoll + " rolls of pennies.");
 console.log(pennies + " unrolled pennies.");
 console.log("Dollar amount of pennies: " + formatter.format(totalPenniesValue));
 
-while (nickels > 40){
-    nickels = nickels -40;
-    nickelsRoll ++;
+while (nickels > 40) {
+    nickels = nickels - 40;
+    nickelsRoll++;
 }
-console.log(nickelsRoll + " rolls of nickels." );
+console.log(nickelsRoll + " rolls of nickels.");
 console.log(nickels + " unrolled nickels.");
 console.log("Dollar amount of nickels: " + formatter.format(totalNickelsValue));
 
-while (dimes > 50){
-    dimes = dimes -50;
-    dimesRoll ++;
+while (dimes > 50) {
+    dimes = dimes - 50;
+    dimesRoll++;
 }
-console.log(dimesRoll + " rolls of dimes." );
+console.log(dimesRoll + " rolls of dimes.");
 console.log(dimes + " unrolled dimes.");
 console.log("Dollar amount of dimes: " + formatter.format(totalDimesValue));
 
-while (quarters > 40){
-    quarters = quarters -40;
-    quartersRoll ++;
+while (quarters > 40) {
+    quarters = quarters - 40;
+    quartersRoll++;
 }
-console.log(quartersRoll + " rolls of quarters." );
+console.log(quartersRoll + " rolls of quarters.");
 console.log(quarters + " unrolled quarters.");
 console.log("Dollar amount of quarters: " + formatter.format(totalQuartersValue));
 
